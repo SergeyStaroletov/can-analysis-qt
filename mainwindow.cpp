d6c022dadb713a8d947bd8cadc246eb6def55f56
Sergey Staroletov <sergey@Air-Sergey.Dlink>
2018-04-11 12:51:41 +0700

    two columns data placement

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,9 +22,9 @@ struct OneCANDisplayNode {
 
 };
 
-const int nodeCountHalf = 20;
+const int nodeCountHalf = 25;
 
-OneCANDisplayNode nodes[nodeCountHalf];
+OneCANDisplayNode nodes[nodeCountHalf * 2];
 
 
 const int offsetX = 5;
@@ -39,7 +39,7 @@ void MainWindow::createControls() {
 
     int oneHeight = maxHeight / nodeCountHalf;
 
-
+    //first column
     for (int i = 0; i < nodeCountHalf; i++) {
 
         nodes[i].editId = new QLineEdit(ui->groupBoxData);
@@ -67,14 +67,51 @@ void MainWindow::createControls() {
         this->layout()->addWidget(nodes[i].labelTime);
 
 
-        nodes[i].checkIsRepeat = new QCheckBox("Repeat", ui->groupBoxData);
+        nodes[i].checkIsRepeat = new QCheckBox("Rep.", ui->groupBoxData);
         nodes[i].checkIsRepeat->setGeometry(offsetX + ui->groupBoxData->x() + 440,
                                      offsetY + ui->groupBoxData->y() + oneHeight * i,
                                      70, 21);
         this->layout()->addWidget(nodes[i].checkIsRepeat);
-
-
     }
+
+    //second column
+
+    const int addWidth = ui->groupBoxData->width() / 2;
+
+    for (int i = 0; i < nodeCountHalf; i++) {
+
+        nodes[i + nodeCountHalf].editId = new QLineEdit(ui->groupBoxData);
+        nodes[i + nodeCountHalf].editId->setGeometry(offsetX + ui->groupBoxData->x() + addWidth,
+                                     offsetY + ui->groupBoxData->y() + oneHeight * i,
+                                     50, 21);
+        this->layout()->addWidget(nodes[i + nodeCountHalf].editId);
+
+        nodes[i + nodeCountHalf].editData = new QLineEdit(ui->groupBoxData);
+        nodes[i + nodeCountHalf].editData->setGeometry(offsetX + ui->groupBoxData->x() + 70 + addWidth,
+                                     offsetY + ui->groupBoxData->y() + oneHeight * i,
+                                     220, 21);
+        this->layout()->addWidget(nodes[i + nodeCountHalf].editData);
+
+        nodes[i + nodeCountHalf].editDecode = new QLineEdit(ui->groupBoxData);
+        nodes[i + nodeCountHalf].editDecode->setGeometry(offsetX + ui->groupBoxData->x() + 300 + addWidth,
+                                     offsetY + ui->groupBoxData->y() + oneHeight * i,
+                                     80, 21);
+        this->layout()->addWidget(nodes[i + nodeCountHalf].editDecode);
+
+        nodes[i + nodeCountHalf].labelTime = new QLabel("0s", ui->groupBoxData);
+        nodes[i + nodeCountHalf].labelTime->setGeometry(offsetX + ui->groupBoxData->x() + 390 + addWidth,
+                                     offsetY + ui->groupBoxData->y() + oneHeight * i,
+                                     50, 21);
+        this->layout()->addWidget(nodes[i + nodeCountHalf].labelTime);
+
+
+        nodes[i + nodeCountHalf].checkIsRepeat = new QCheckBox("Rep.", ui->groupBoxData);
+        nodes[i + nodeCountHalf].checkIsRepeat->setGeometry(offsetX + ui->groupBoxData->x() + 440 + addWidth,
+                                     offsetY + ui->groupBoxData->y() + oneHeight * i,
+                                     70, 21);
+        this->layout()->addWidget(nodes[i + nodeCountHalf].checkIsRepeat);
+    }
+
 
 
 

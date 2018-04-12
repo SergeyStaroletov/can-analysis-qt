d1996629af06cd8581889ec1db3f3554ce085ecc
Sergey <serg_soft@mail.ru>
2018-04-12 08:29:07 +0700

    Arduino code formating

--- Arduino/Test/sketch_can_tx/sketch_can_tx.ino
+++ Arduino/Test/sketch_can_tx/sketch_can_tx.ino
@@ -11,52 +11,44 @@
 // v0.9b and v1.0 is default D10
 const int SPI_CS_PIN = 10;
 
-
 int num = 0;
 int index = 0;
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+MCP_CAN CAN(SPI_CS_PIN);  // Set CS pin
 
-void setup()
-{
-    Serial.begin(115200);
-
-    
+void setup() {
+  Serial.begin(115200);
 
 START_INIT:
 
-    if(CAN_OK == CAN.begin(CAN_500KBPS, MCP_8MHz))                   // init can bus : baudrate = 500k
-    {
-        Serial.println("CAN BUS Shield init ok!");
-    }
-    else
-    {
-        Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
-        delay(100);
-        goto START_INIT;
-    }
+  if (CAN_OK ==
+      CAN.begin(CAN_500KBPS, MCP_8MHz))  // init can bus : baudrate = 500k
+  {
+    Serial.println("CAN BUS Shield init ok!");
+  } else {
+    Serial.println("CAN BUS Shield init fail");
+    Serial.println("Init CAN BUS Shield again");
+    delay(100);
+    goto START_INIT;
+  }
 }
 
-
-void loop()
-{
+void loop() {
   unsigned char len = 0;
   unsigned char buf[8];
 
-  num = random (255);
+  num = random(255);
 
   unsigned char stmp[8] = {'h', 'e', 'l', 'l', 'o', ' ', 's', 'e'};
 
-  //send generated message
+  // send generated message
   CAN.sendMsgBuf(num, 0, 8, stmp);
 
-  //delay(1000);
+  // delay(1000);
 
   index = random(8);
 
   stmp[index]++;
   if (stmp[index]++ > 255) stmp[index] = 0;
-  
 }
 
 /*********************************************************************************************************
